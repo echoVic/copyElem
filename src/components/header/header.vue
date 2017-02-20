a8b4cf5d6776573a1c67ea98e69bcb5c89f50ebd
echovic <137844255@qq.com>
2017-02-21 00:29:04 +0800

    header外部组件completed

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -22,7 +22,13 @@
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-tittle"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
   </div>
 </template>
 
@@ -43,8 +49,9 @@
   @import "../../common/stylus/mixin";
 
   .header
+    position: relative
     color: #fff
-    background-color: #999
+    background: rgba(7, 17, 27, 0.5)
     .content-wrapper
       position: relative
       padding: 24px 12px 18px 24px
@@ -119,6 +126,46 @@
           margin-left: 2px
           line-height: 24px
           font-size: 10px
+
+
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7, 17, 27, 0.2)
+      .bulletin-tittle
+        display: inline-block
+        vertical-align: top
+        margin-top: 7px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        vertical-align: top
+        margin: 0 4px
+        font-size: 10px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: 10px
+        right: 12px
+        top: 8px
+
+
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
+
 
 
 </style>
